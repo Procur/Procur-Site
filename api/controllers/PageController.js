52543f2f6d813498f5763db33b1957611e0c3c50
Will Ricketts <will@willricketts.com>
2014-06-17 11:12:08 -0400

    added controller actions and views

--- api/controllers/PageController.js
+++ api/controllers/PageController.js
@@ -6,6 +6,37 @@
  */
 
 module.exports = {
-	
+
+  aboutUs: function(req, res){
+    res.view();
+  },
+  careers: function(req, res){
+    res.view();
+  },
+  contact: function(req, res){
+    res.view();
+  },
+  support: function(req, res){
+    res.view();
+  },
+  faq: function(req, res){
+    res.view();
+  },
+  features: function(req, res){
+    res.view();
+  },
+  pricing: function(req, res){
+    res.view();
+  },
+  privacy: function(req, res){
+    res.view();
+  },
+  termsOfUse: function(req, res){
+    res.view();
+  },
+  termsOfService: function(req, res){
+    res.view();
+  }
+
 };
 
